8eda471ea057c0880c0930300f442701e7d2d783
jordiauger <jordi0189@gmail.com>
2020-04-10 21:19:32 +0200

    Mapa amb points i formulari
    , es mostra la info cutre

--- src/pages/create-request/create-request.ts
+++ src/pages/create-request/create-request.ts
@@ -5,6 +5,18 @@ import {LoginService} from "../../services/login.service";
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
 import {Request} from "../../models/request";
 
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import {defaults as defaultControls, OverviewMap} from 'ol/control';
+import {interaction as defaultInteractions} from 'ol/interaction';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import {Circle as CircleStyle, Fill, Stroke, Style, Text, Icon} from 'ol/style';
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
+
 @Component({
   templateUrl: './create-request.html'
 })
@@ -12,18 +24,56 @@ export class CreateRequestPage implements OnInit{
 
   form: FormGroup;
   private requestsCollection: AngularFirestoreCollection<Request>;
+  private map: Map;
+  private markersLayer:VectorLayer;
+  private chosenCoordinates:any;
 
   constructor(public viewCtrl: ViewController, private loginService: LoginService, private afs: AngularFirestore, private fb: FormBuilder) {
+    
     this.form = this.fb.group({
       'title': ['', [Validators.required]],
       'description': ['', [Validators.required]],
-      'deliveryDate': [''],
-      'lat': ['', [Validators.required]],
-      'lon': ['', [Validators.required]]
+      'deliveryDate': ['']
     })
   }
 
   ngOnInit(): void {
+    this.map = new Map({
+      target: 'mapSelector',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [313986.42, 5158087.34],
+        zoom: 14
+      })
+    });
+    this.map.on('click', (evt)=> {
+        if (this.markersLayer) this.markersLayer.getSource().clear(true);
+        this.chosenCoordinates = evt.coordinate;
+        alert("You clicked near LON " + this.chosenCoordinates[0] + ", LAT: "+ this.chosenCoordinates[1] + " E");
+        var iconFeature = new Feature({
+            geometry: new Point(this.chosenCoordinates)
+        });
+        var iconStyle = new Style({
+            image: new Icon(({
+                anchor: [0.5, 1],
+                src: "http://cdn.mapmarker.io/api/v1/pin?text=P&size=50&hoffset=1"
+            }))
+        });
+
+        iconFeature.setStyle(iconStyle);
+        this.markersLayer = new VectorLayer({
+          source: new VectorSource({
+              features: [iconFeature]
+          })
+        });
+    });
+
     this.requestsCollection = this.afs.collection<Request>('/requests');
   }
 
@@ -33,7 +83,9 @@ export class CreateRequestPage implements OnInit{
     }
 
     const newRequest: Request = {
+      //TODO:: CAMBIAR EL LOCATION PER LA GEOLOCATION AQUELLA?
       ...this.form.getRawValue(),
+      location: this.chosenCoordinates,
       createdAt: new Date(),
       createdBy: this.loginService.getCurrentUser().uid,
       status: "pending"
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,18 @@ import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import {defaults as defaultControls, OverviewMap} from 'ol/control';
 import {interaction as defaultInteractions} from 'ol/interaction';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import {Circle as CircleStyle, Fill, Stroke, Style, Text, Icon} from 'ol/style';
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
+
+import {AngularFirestore} from "@angular/fire/firestore";
+import {AngularFirestoreCollection} from "@angular/fire/firestore";
+import {Request} from "../../models/request";
+import {Observable} from "rxjs";
+
+
 
 @Component({
   selector: 'page-home',
@@ -15,8 +27,12 @@ import {interaction as defaultInteractions} from 'ol/interaction';
 export class HomePage implements OnInit {
 
   private map: Map;
+  private markersLayer: VectorLayer;
+  private selectedRequest: any;
+  protected requestCollection: AngularFirestoreCollection<Request>;
+  protected requests: Observable<Request[]>;
 
-  constructor(private modalCtrl: ModalController, private toastController: ToastController) {
+  constructor(private modalCtrl: ModalController, private toastController: ToastController, private afs: AngularFirestore) {
   }
 
   ngOnInit(): void {
@@ -34,6 +50,52 @@ export class HomePage implements OnInit {
         center: [313986.42, 5158087.34],
         zoom: 14
       })
+    });
+
+
+    this.markersLayer = new VectorLayer({
+        source: new VectorSource({
+            features: []
+        })
+    });
+    this.map.addLayer(this.markersLayer);
+
+
+    this.afs.collection('requests').valueChanges().subscribe((requests:Request[])=>{
+        let features = [];
+        for (var i in requests){
+            let req = requests[i];
+            console.log(req)
+            /*var longitude = req.location;
+            var latitude = req.location;*/
+
+            var iconFeature = new Feature({
+                geometry: new Point(this.randomGeo(313986.42, 5158087.34))
+            });
+
+            var iconStyle = new Style({
+                image: new Icon(({
+                    anchor: [0.5, 1],
+                    src: "http://cdn.mapmarker.io/api/v1/pin?text=P&size=50&hoffset=1"
+                }))
+            });
+
+            iconFeature.setStyle(iconStyle);
+            iconFeature["request"] = req;
+            features.push(iconFeature);
+        }
+        this.markersLayer.getSource().clear(true);
+        this.markersLayer.getSource().addFeatures(features);
+    });
+
+    
+    this.map.on('click', (evt)=> {
+        var feature = this.map.forEachFeatureAtPixel(evt.pixel, function(feature) {
+          return feature;
+        });
+        console.log(feature);
+        if (feature && feature["request"]) this.selectedRequest = feature["request"];
+        else this.selectedRequest = null;
     });
   }
 
@@ -56,4 +118,14 @@ export class HomePage implements OnInit {
 
     });
   }
+
+  randomGeo(lon, lat) {
+      var u = Math.random();
+
+      if (u < 0.5)
+        return [ lon - Math.random()*1000 ,  lat - Math.random()*1000] 
+      else 
+        return [ lon + Math.random()*1000 ,  lat + Math.random()*1000] 
+  }
+
 }
