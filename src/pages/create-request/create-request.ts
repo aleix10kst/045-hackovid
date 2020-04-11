75c63ecec1f80e8556b051a932b40458aaef229a
Aleix Canet Vidal <acanet94@gmail.com>
2020-04-12 00:21:43 +0200

    Refactoritzada part de mapes

--- src/pages/create-request/create-request.ts
+++ src/pages/create-request/create-request.ts
@@ -1,35 +1,26 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {ViewController} from "ionic-angular";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
 import {Request} from "../../models/request";
-import Geolocation from 'ol/Geolocation';
-
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import {Style, Icon} from 'ol/style';
-import Point from 'ol/geom/Point';
-import Feature from 'ol/Feature';
-import {toLonLat} from 'ol/proj';
 import * as firebaseApp from 'firebase/app';
 import * as geofirex from 'geofirex';
-import {GeoFireClient} from "geofirex";
+import {GeoFireClient} from 'geofirex';
 import {UserSevice} from "../../services/user.sevice";
+import {GoogleMap, GoogleMapOptions, GoogleMaps, GoogleMapsEvent, LatLng} from "@ionic-native/google-maps"
+import {GeolocationPosition, Plugins} from '@capacitor/core';
+
+const {Geolocation} = Plugins;
 
 @Component({
   templateUrl: './create-request.html'
 })
-export class CreateRequestPage implements OnInit{
+export class CreateRequestPage implements OnInit, OnDestroy {
 
   form: FormGroup;
   private requestsCollection: AngularFirestoreCollection<Request>;
-  private map: Map;
-  private markersLayer:VectorLayer;
-  private chosenCoordinates:any;
+  private map: GoogleMap;
+  private chosenCoordinates: {lat: number, lon: number};
 
   private geofireClient: GeoFireClient;
 
@@ -45,66 +36,16 @@ export class CreateRequestPage implements OnInit{
   }
 
   ngOnInit(): void {
-    this.map = new Map({
-      target: 'mapSelector',
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
-        })
-      ],
-      view: new View({
-        center: [313986.42, 5158087.34],
-        zoom: 14
-      })
-    });
-    this.markersLayer = new VectorLayer({
-      source: new VectorSource({
-          features: []
-      })
-    });
-    this.map.addLayer(this.markersLayer);
-    this.map.on('click', (evt)=> {
-        this.markersLayer.getSource().clear(true);
-        this.chosenCoordinates = toLonLat(evt.coordinate);
-        var iconFeature = new Feature({
-            geometry: new Point(this.chosenCoordinates)
-        });
-        var iconStyle = new Style({
-            image: new Icon(({
-                anchor: [0.5, 1],
-                src: "http://cdn.mapmarker.io/api/v1/pin?text=P&size=50&hoffset=1"
-            }))
-        });
-
-        iconFeature.setStyle(iconStyle);
-        this.markersLayer.getSource().addFeatures([iconFeature]);
-
-    });
-    let firstTime = true;
-
-    var geolocation = new Geolocation({
-      // enableHighAccuracy must be set to true to have the heading value.
-      tracking:true,
-      trackingOptions: {
-        enableHighAccuracy: true,
-        maximumAge:2000
-      },
-      projection: this.map.getView().getProjection()
-    });
-
-    geolocation.on('change', ()=>{
-
-        var pos = geolocation.getPosition();
-        if (firstTime){
-          firstTime = false;
-          this.center(pos[0],pos[1]);
-        }
-        this.addMarkerPosition('myPos', pos[0], pos[1], 'assets/imgs/myPos.svg');
-    })
-
     this.requestsCollection = this.afs.collection<Request>('/requests');
+  }
+
+  ionViewWillEnter(): void {
+    this.initalizeMap();
+  }
+
+
+  cancel(): void {
+    this.viewCtrl.dismiss('canceled');
   }
 
   save(): void {
@@ -115,7 +56,7 @@ export class CreateRequestPage implements OnInit{
     const newRequest: Request = {
       ...this.form.getRawValue(),
       uuid: `${this.userService.getCurrentUser().uid}-${new Date().getTime()}`,
-      location: this.geofireClient.point(this.chosenCoordinates[1], this.chosenCoordinates[0]),
+      location: this.geofireClient.point(this.chosenCoordinates.lat, this.chosenCoordinates.lon),
       createdAt: new Date().getTime(),
       createdBy: this.userService.getCurrentUser().uid,
       status: "pending"
@@ -123,44 +64,34 @@ export class CreateRequestPage implements OnInit{
     this.requestsCollection.doc(newRequest.uuid).set(newRequest).then(() => this.viewCtrl.dismiss('created'));
   }
 
-  cancel(): void {
-    this.viewCtrl.dismiss('canceled');
-  }
-
-  addMarkerPosition(name, lng, lat, img) {
-    let style = new Icon({
-        src: img,
-        scale: 1,
-        rotation: 0,
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        opacity: 1
-    })
-    var feature = new Feature({
-        geometry: new Point([lng, lat]),
-    })
-
-    feature.set('name', name)
-
-    feature.setStyle(new Style({
-        image: style
-    }))
-    var layer = new VectorLayer({
-        source: new VectorSource({
-            features: [feature]
-        })
-    })
-    layer.set('name', name)
-    layer.setZIndex(10)
-    this.map.addLayer(layer);
-}
-
-center(lon, lat) {
-    let feature = new Feature({
-        geometry: new Point([lon, lat]),
+  private initalizeMap(): void {
+    Geolocation.getCurrentPosition().then((coordinates: GeolocationPosition) => {
+      let mapOptions: GoogleMapOptions = {
+        camera: {
+          target: {
+            lat: coordinates.coords.latitude,
+            lng: coordinates.coords.longitude
+          },
+          zoom: 18,
+          tilt: 30
+        }
+      };
+      this.map = GoogleMaps.create('mapRequest', mapOptions);
+      this.map.on(GoogleMapsEvent.MAP_CLICK).subscribe(([latLng]: [LatLng]) => {
+        this.chosenCoordinates = {lat: latLng.lat, lon: latLng.lng};
+        this.map.clear();
+        this.map.addMarker({
+          title: '',
+          position: {
+            lat: latLng.lat,
+            lng: latLng.lng
+          }
+        });
+      });
     });
-    this.map.getView().fit(feature.getGeometry());
-    this.map.getView().setZoom(14);
   }
 
+  ngOnDestroy(): void {
+    this.map.off(GoogleMapsEvent.MAP_CLICK);
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,44 +1,43 @@
-import {Component, OnInit, AfterViewInit} from '@angular/core';
-import {Alert, AlertController, ModalController, ToastController} from 'ionic-angular';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {Alert, AlertController, ModalController, Platform, ToastController} from 'ionic-angular';
 import {CreateRequestPage} from "../create-request/create-request";
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
 import {Request} from "../../models/request";
 import {UserSevice} from "../../services/user.sevice";
 import * as firebaseApp from 'firebase/app';
-import { Platform } from 'ionic-angular';
 import * as geofirex from 'geofirex';
 import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  Marker,
   Environment,
+  GoogleMap,
+  GoogleMapOptions,
+  GoogleMaps,
+  GoogleMapsEvent,
   ILatLng,
+  Marker,
   MarkerIcon
 } from '@ionic-native/google-maps';
-import { Plugins } from '@capacitor/core';
-const { Geolocation } = Plugins;
+import {GeolocationPosition, Plugins} from '@capacitor/core';
+import {GeoFireClient, GeoQueryDocument} from "geofirex";
+import {FirePoint} from "geofirex/dist/client";
 
+const {Geolocation} = Plugins;
 
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnInit, AfterViewInit {
-
-  private requestCollection: AngularFirestoreCollection<Request>;
-  private selectedRequest: Request;
-  private geo: any;
-  private map: GoogleMap;
-  protected radius: number;
-  private currentPoint: any;
-  private markers:Marker[];
-
-  private markerPosition: Marker;
+export class HomePage implements OnInit, AfterViewInit, OnDestroy {
 
   canAddRequests: boolean;
+  protected radius: number;
+  private requestCollection: AngularFirestoreCollection<Request>;
+  private selectedRequest: Request;
+  private geo: GeoFireClient;
+  private map: GoogleMap;
+  private currentPoint: FirePoint;
+  private markers: Marker[];
+  private markerPosition: Marker;
 
   constructor(
     private modalCtrl: ModalController,
@@ -47,62 +46,24 @@ export class HomePage implements OnInit, AfterViewInit {
     private userSevice: UserSevice,
     private platform: Platform,
     private afs: AngularFirestore) {
-      this.geo = geofirex.init(firebaseApp);
+    this.geo = geofirex.init(firebaseApp);
   }
 
-  ngAfterViewInit():void{
+  ngAfterViewInit(): void {
     this.radius = 10;
     this.markers = [];
   }
 
   ngOnInit(): void {
-    this.platform.ready().then(()=>{
+    this.requestCollection = this.afs.collection('requests');
+    this.canAddRequests = this.userSevice.isSuperUser() || this.userSevice.isEntitatUser();
+
+    this.platform.ready().then(() => {
       Environment.setEnv({
         'API_KEY_FOR_BROWSER_RELEASE': 'XXXXXXXXXXXXXXXXXXXXXXXX',
         'API_KEY_FOR_BROWSER_DEBUG': 'XXXXXXXXXXXXXXXXXXXXXXXXX'
       });
-
-
-      let firstTime = true;
-      this.requestCollection = this.afs.collection('requests');
-      this.canAddRequests = this.userSevice.isSuperUser() || this.userSevice.isEntitatUser();
-
-      let mapOptions: GoogleMapOptions = {
-        camera: {
-           target: {
-             lat: 43.0741904,
-             lng: 2
-           },
-           zoom: 18,
-           tilt: 30
-         }
-      };
-
-      this.map = GoogleMaps.create('map', mapOptions);
-
-      let icon = {} as MarkerIcon;
-      icon["size"] = {};
-      icon["url"] = 'assets/imgs/myPos.svg';
-      icon["size"]["width"] = 40;
-      icon["size"]["height"] = 40;
-
-      this.markerPosition = this.map.addMarkerSync({
-          icon: icon,
-          position: {
-            lat: 43.0741904,
-            lng: 2
-          }
-      });
-
-      Geolocation.watchPosition({}, (position, err) => {
-          if (firstTime){
-            firstTime = false;
-            this.center(position.coords.longitude,position.coords.latitude);
-          }
-          this.updateMarkerPosition(position.coords.longitude,position.coords.latitude);
-          this.currentPoint = this.geo.point(position.coords.longitude,position.coords.latitude);
-          this.paintMap();
-      });
+      this.initializeMap();
     });
   }
 
@@ -126,15 +87,6 @@ export class HomePage implements OnInit, AfterViewInit {
     });
   }
 
-  randomGeo(lon, lat) {
-    var u = Math.random();
-
-    if (u < 0.5)
-      return [lon - Math.random() * 1000, lat - Math.random() * 1000];
-    else
-      return [lon + Math.random() * 1000, lat + Math.random() * 1000]
-  }
-
   updateMarkerPosition(lng, lat) {
     let ltlng = {} as ILatLng;
     ltlng["lat"] = lat;
@@ -149,142 +101,170 @@ export class HomePage implements OnInit, AfterViewInit {
     this.map.setCameraTarget(ltlng);
   }
 
-    valueChanged($event){
-      this.paintMap();
-    }
+  valueChanged() {
+    this.paintMap();
+  }
 
-
-    paintMap(){
-      let point = this.currentPoint;
-      if (!point){
-        let coordsConverted = [2, 41];
-        this.currentPoint = this.geo.point(coordsConverted[0],coordsConverted[1]);
-        point = this.currentPoint;
-      }
-      this.geo.query('requests').within(point, this.radius, 'location').subscribe((requests: Request[]) => {
-          for (var i in requests) {
-                let req = requests[i];
-                let iconsrc = "http://cdn.mapmarker.io/api/v1/pin?text=P&size=50&hoffset=1&background=FACF1B";//groc
-                if (req.status === "accepted"){
-                  iconsrc = "http://cdn.mapmarker.io/api/v1/pin?text=A&size=50&hoffset=1&background=598BF7";//blau
-                }else if (req.status ===  "completed"){
-                  iconsrc = "http://cdn.mapmarker.io/api/v1/pin?text=C&size=50&hoffset=1&background=0EE548";//verd
-                }
-                let icon = {} as MarkerIcon;
-                icon["url"] = iconsrc;
-                icon["size"] = {};
-                icon["size"]["width"] = 40;
-                icon["size"]["height"] = 40;
-
-                let marker = this.map.addMarkerSync({
-                  icon: icon,
-                  animation: 'DROP',
-                  position: {
-                    lat: req.location.geopoint.latitude,
-                    lng: req.location.geopoint.longitude
-                  }
-                });
-                marker["request"]=req;
-                marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe((marker)=>{
-                  this.selectedRequest = marker["request"];
-                  let alert: Alert;
-                  switch (this.selectedRequest.status) {
-                    case 'pending':
-                      alert = this.alertCtrl.create({
-                        title: this.selectedRequest.title,
-                        message: this.selectedRequest.description,
-                        buttons: [
-                          {
-                            text: 'Cancel·la'
-                          },
-                          {
-                            text: 'Accepta la petició',
-                            handler: () => {
-                              this.requestCollection.doc(this.selectedRequest.uuid).update({
-                                status: 'accepted',
-                                acceptedBy: this.userSevice.getCurrentUser().uid,
-                                acceptedAt: new Date().getTime()
-                              }).then(() => {
-                                const acceptedRequestToast = this.toastController.create({
-                                  message: 'Has acceptat la petició.',
-                                  duration: 3000
-                                });
-                                acceptedRequestToast.present();
-                              });
-                            }
-                          }
-                        ]
-                      });
-                      alert.present();
-                      break;
-                    case 'accepted':
-                      alert = this.alertCtrl.create({
-                        title: this.selectedRequest.title,
-                        message: this.selectedRequest.description,
-                        buttons: [
-                          {
-                            text: 'Completa',
-                            handler: () => {
-                              this.requestCollection.doc(this.selectedRequest.uuid).update({
-                                status: 'completed',
-                              }).then(() => {
-                                const completedRequestToast = this.toastController.create({
-                                  message: 'Has completat la petició',
-                                  duration: 3000
-                                });
-                                completedRequestToast.present();
-                              })
-                            }
-                          },
-                          {
-                            text: 'Rebutja',
-                            handler: () => {
-                              this.requestCollection.doc(this.selectedRequest.uuid).update({
-                                status: 'pending',
-                                acceptedBy: null,
-                                acceptedAt: null
-                              }).then(() => {
-                                const canceledRequestToast = this.toastController.create({
-                                  message: 'Has cancel·lat la petició',
-                                  duration: 3000
-                                });
-                                canceledRequestToast.present();
-                              })
-                            }
-                          },
-                          {
-                            text: 'Tanca'
-                          }
-                        ]
-                      });
-                      alert.present();
-                      break;
-                  }
-                })
-                this.markers.push(marker);
-            }
-        });
-    }
-
-
-
-    // Sets the map on all markers in the array.
-    setMapOnAll(map) {
-      for (var i = 0; i < this.markers.length; i++) {
-        this.markers[i].remove();
-      }
-    }
-
-    // Removes the markers from the map, but keeps them in the array.
-    clearMarkers() {
-      this.setMapOnAll(null);
-    }
-
-    // Deletes all markers in the array by removing references to them.
-    deleteMarkers() {
+  paintMap() {
+    this.geo.query('requests').within(this.currentPoint, this.radius, 'location').subscribe((requests: GeoQueryDocument[]) => {
       this.clearMarkers();
-      this.markers = [];
+      console.log(requests);
+      requests.forEach((request: any) => {
+        let iconsrc = "http://cdn.mapmarker.io/api/v1/pin?text=P&size=50&hoffset=1&background=FACF1B";//groc
+        if (request.status === "accepted") {
+          iconsrc = "http://cdn.mapmarker.io/api/v1/pin?text=A&size=50&hoffset=1&background=598BF7";//blau
+        } else if (request.status === "completed") {
+          iconsrc = "http://cdn.mapmarker.io/api/v1/pin?text=C&size=50&hoffset=1&background=0EE548";//verd
+        }
+        let icon = {} as MarkerIcon;
+        icon["url"] = iconsrc;
+        icon["size"] = {};
+        icon["size"]["width"] = 40;
+        icon["size"]["height"] = 40;
+
+        let marker = this.map.addMarkerSync({
+          icon: icon,
+          animation: 'DROP',
+          position: {
+            lat: request.location.geopoint.latitude,
+            lng: request.location.geopoint.longitude
+          }
+        });
+        marker["request"] = request;
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(([,marker]) => {
+          this.selectedRequest = marker["request"];
+          let alert: Alert;
+          switch (this.selectedRequest.status) {
+            case 'pending':
+              alert = this.alertCtrl.create({
+                title: this.selectedRequest.title,
+                message: this.selectedRequest.description,
+                buttons: [
+                  {
+                    text: 'Cancel·la'
+                  },
+                  {
+                    text: 'Accepta la petició',
+                    handler: () => {
+                      this.requestCollection.doc(this.selectedRequest.uuid).update({
+                        status: 'accepted',
+                        acceptedBy: this.userSevice.getCurrentUser().uid,
+                        acceptedAt: new Date().getTime()
+                      }).then(() => {
+                        const acceptedRequestToast = this.toastController.create({
+                          message: 'Has acceptat la petició.',
+                          duration: 3000
+                        });
+                        acceptedRequestToast.present();
+                      });
+                    }
+                  }
+                ]
+              });
+              alert.present();
+              break;
+            case 'accepted':
+              alert = this.alertCtrl.create({
+                title: this.selectedRequest.title,
+                message: this.selectedRequest.description,
+                buttons: [
+                  {
+                    text: 'Completa',
+                    handler: () => {
+                      this.requestCollection.doc(this.selectedRequest.uuid).update({
+                        status: 'completed',
+                      }).then(() => {
+                        const completedRequestToast = this.toastController.create({
+                          message: 'Has completat la petició',
+                          duration: 3000
+                        });
+                        completedRequestToast.present();
+                      })
+                    }
+                  },
+                  {
+                    text: 'Rebutja',
+                    handler: () => {
+                      this.requestCollection.doc(this.selectedRequest.uuid).update({
+                        status: 'pending',
+                        acceptedBy: null,
+                        acceptedAt: null
+                      }).then(() => {
+                        const canceledRequestToast = this.toastController.create({
+                          message: 'Has cancel·lat la petició',
+                          duration: 3000
+                        });
+                        canceledRequestToast.present();
+                      })
+                    }
+                  },
+                  {
+                    text: 'Tanca'
+                  }
+                ]
+              });
+              alert.present();
+              break;
+          }
+        });
+        this.markers.push(marker);
+      })
+    });
+  }
+
+
+  // Sets the map on all markers in the array.
+  setMapOnAll(map) {
+    for (let i = 0; i < this.markers.length; i++) {
+      this.markers[i].remove();
     }
+  }
 
+  // Removes the markers from the map, but keeps them in the array.
+  clearMarkers() {
+    this.setMapOnAll(null);
+  }
 
+  // Deletes all markers in the array by removing references to them.
+  deleteMarkers() {
+    this.clearMarkers();
+    this.markers = [];
+  }
+
+  private initializeMap(): void {
+    Geolocation.getCurrentPosition().then((position: GeolocationPosition) => {
+      this.currentPoint = this.geo.point(position.coords.latitude, position.coords.longitude);
+      const mapOptions: GoogleMapOptions = {
+        camera: {
+          target: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          },
+          zoom: 18,
+          tilt: 30
+        }
+      };
+
+      this.map = GoogleMaps.create('map', mapOptions);
+
+      let icon = {} as MarkerIcon;
+      icon["size"] = {};
+      icon["url"] = 'assets/imgs/myPos.svg';
+      icon["size"]["width"] = 40;
+      icon["size"]["height"] = 40;
+
+      this.markerPosition = this.map.addMarkerSync({
+        icon: icon,
+        position: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+      });
+      this.paintMap();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.map.off(GoogleMapsEvent.MARKER_CLICK);
+  }
 }
